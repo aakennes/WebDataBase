700f850f2b0492a102c7a03166e4ff7db8b33c17
aakennes <rsq363570@outlook.com>
2024-12-23 19:35:24 +0800

    update frontendBack Start

--- data/team/frontend/frontendBack/server.js
+++ data/team/frontend/frontendBack/server.js
@@ -58,28 +58,6 @@ app.get('/api/courses', (req, res) => {
     });
   });
 
-
-// // 模拟数据
-// const data = {
-//   courses: [
-//     { id: 1, name: "高级语言程序设计", status: "未加入" },
-//     { id: 2, name: "数据结构", status: "已加入" },
-//     { id: 3, name: "操作系统", status: "未加入" },
-//     { id: 4, name: "计算机网络", status: "已加入" },
-//     { id: 5, name: "人工智能", status: "未加入" },
-//     { id: 6, name: "编译原理", status: "未加入" },
-//     { id: 7, name: "并行程序设计", status: "未加入" }
-//   ],
-//   exams: [
-//     { id: 1, type: "考试", name: "高级语言程序设计2-2上机考试", time: "2023/6/8 17:14:59" },
-//     { id: 2, type: "考试", name: "高级语言程序设计2-1上机考试", time: "2023/2/25 21:49:59" }
-//   ],
-//   competitions: [
-//     { id: 1, type: "竞赛", name: "NKPC19-现场赛", time: "2023/4/22 16:59:59" },
-//     { id: 2, type: "竞赛", name: "NKPC19-网络赛", time: "2023/4/19 18:59:59" }
-//   ]
-// };
-
 // // API 路由：获取数据
 // app.get('/api/data', (req, res) => {
 //   res.json(data); // 返回 JSON 数据
